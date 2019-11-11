6838b77c7df369993ee811054763227f2b170916
unknown <chaserichards01@gmail.com>
2019-11-10 19:06:08 -0600

    task12 complete working on task3

--- task3Test.py
+++ task3Test.py
@@ -6,41 +6,45 @@ from cryptography.hazmat.primitives import hashes
 import os
 import time
 def guess_passwordHash(returnline):
-    chars = string.ascii_lowercase
-    attempts = 0
-    password_length = 0
-    for password_length in range(3, 8):
-        for guess in itertools.product(chars, repeat=password_length):
-            attempts += 1
-            guess = ''.join(guess)
-            def encrypt_string(hash_string):
-                sha_signature = \
-                    hashlib.SHA3_256(hash_string.encode()).hexdigest()
-                return sha_signature
-            hash_string = guess
-            sha_signature = encrypt_string(hash_string)
-            if sha_signature == returnline:
-                return 'password is {}. found in {} guesses.'.format(guess, attempts)
-            print (guess,attempts)
-
-
+	userPassword = line.split(":", 1)[1]
+	chars = userPassword.encode("ascii")
+	attempts = 0
+	password_length = 0
+	for password_length in range(3, 8):
+		for guess in itertools.product(chars, repeat=password_length):
+			attempts += 1
+			guess = ''.join(guess)
+			def encrypt_string(hash_string):
+				digest = hashes.Hash(hashes.SHA3_256(), backend = openssl.backend)
+				digest.update(chars)
+				hashedpsswrd = digest.finalize()
+				hashedpsswrd = hashedpsswrd.hex()
+				return hashedpsswrd
+			hash_string = guess
+			hashedpsswrd = encrypt_string(hash_string)
+			if hashedpsswrd == returnline:
+				return 'password is {}. found in {} guesses.'.format(guess, attempts)
+			print (guess,attempts)
 
 
 def guess_passwordHashSalt(returnline):
-    chars = string.ascii_lowercase
+    saltyPassword = line.split(":", 1)[1]
+    chars = saltyPassword.encode("ascii")
     attempts = 0
     for password_length in range(3, 8):
         for guess in itertools.product(chars, repeat=password_length):
             attempts += 1
             guess = ''.join(guess)
             def encrypt_string(hash_string):
-                sha_signature = \
-                    hashlib.SHA3_256(hash_string.encode()).hexdigest()
+                digest = hashes.Hash(hashes.SHA3_256(), backend = openssl.backend)
+                digest.update(chars)
+                salthashedpsswrd = digest.finalize()
+                salthashedpsswrd = salthashedpsswrd.hex()
                 return sha_signature
             hash_string = guess + salt
-            sha_signature = encrypt_string(hash_string)
-            print(sha_signature)
-            if sha_signature == returnline:
+            salthashedpsswrd = encrypt_string(hash_string)
+            print(salthashedpsswrd)
+            if salthashedpsswrd == returnline:
                 return 'password is {}. found in {} guesses.'.format(guess, attempts)
             print (guess,attempts)
 
